1b19956c12522a6a484899c68c86715957a4cfac
Francisco Santiago <fcosantiagoc@gmail.com>
2024-03-16 12:49:57 -0600

    Inicializando repaso de java y creando mi primera clase

---
+++ src/main/java/com/francisco/repasojava/structure/MyFirstProgram1.java
@@ -0,0 +1,18 @@
+package com.francisco.repasojava.structure;
+
+/**
+ * The type My first program 1. El nombre de la clase debe empezar siempre por mayúscula, por lo que es recomendable usar siempre
+ * la nomenclatura Pascal Case ex: MyFirstProgram y debe siempre empezar por una letra.
+ * \n
+ * Una clase podemos definirla como una plantilla a utilizar
+ */
+public class MyFirstProgram1 {
+
+    /**
+     * El método main es el que se encarga de ejecutar el programa
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Mi primer programa repasando java");
+    }
+}
---
+++ src/test/java/com/francisco/repasojava/RepasoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco.repasojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasoJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
