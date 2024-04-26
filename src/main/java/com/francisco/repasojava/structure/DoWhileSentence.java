d1b2b5a3e6c2a65476ebf5e9c900fcc08f2b5bb5
Francisco Santiago <fcosantiagoc@gmail.com>
2024-04-25 22:59:03 -0600

    Generando aplicacion generadora de tablas de multipicar con ciclo do while

---
+++ src/main/java/com/francisco/repasojava/structure/DoWhileSentence.java
@@ -0,0 +1,25 @@
+package com.francisco.repasojava.structure;
+
+import java.util.Scanner;
+
+public class DoWhileSentence {
+    /**
+     * El ciclo do while ejecuta el código dentro del bloque
+     * siempre antes de validar la condición colocada, es indispensable
+     * tener en cuenta que la variable colocada en la condición, cambie
+     * de lo contrario nos enfrentaríamos en un bucle infinito
+     */
+    public static void main(String[] args) {
+        int i = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduzca la tabla a generar");
+        int num = scanner.nextInt();
+        System.out.println("Introduzca el limite de la tabla");
+        int limit = scanner.nextInt();
+        do {
+            System.out.println(num + "*"+ i + "=" + (num*i));
+            i++;
+        } while(i<=limit);
+    }
+
+}
